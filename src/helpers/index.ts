3ac815f38a0cb484ebd464834954a297d88e8924
rekmixa <rekmixa@gmail.com>
2025-09-08 23:07:37 +0500

    remove logging errors to admins

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -15,13 +15,13 @@ export function bytesToSize(bytes: number): string {
 }
 
 export function showUptime(): string {
-  function format(time) {
-    function pad(s) {
+  function format(time: number) {
+    function pad(s: number) {
       return (s < 10 ? '0' : '') + s
     }
 
     const hours = Math.floor(time / (60 * 60))
-    const minutes = Math.floor(time % (60 * 60) / 60)
+    const minutes = Math.floor((time % (60 * 60)) / 60)
     const seconds = Math.floor(time % 60)
 
     return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
@@ -31,14 +31,18 @@ export function showUptime(): string {
 }
 
 export function filePutContents(filename: string, content: string): void {
-  require('fs').writeFile(filename, content, function (err) {
+  require('fs').writeFile(filename, content, function(err) {
     if (err) throw err
     console.log("It's saved!")
   })
 }
 
 export function convertTimeZone(date: Date | string, timeZone): Date {
-  return new Date((typeof date === 'string' ? new Date(date) : date).toLocaleString("en-US", { timeZone }))
+  return new Date(
+    (typeof date === 'string' ? new Date(date) : date).toLocaleString('en-US', {
+      timeZone,
+    }),
+  )
 }
 
 export function replaceAll(string: string, search: string, replace: string) {
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,7 +1,6 @@
 import { ConsoleLogger } from '@nestjs/common'
 import { clc } from '@nestjs/common/utils/cli-colors.util'
 import { bytesToSize, showUptime } from 'src/helpers'
-import { sendMessageToAdmin } from 'src/telegram/telegram.module'
 
 declare type messageColor = 'green' | 'red' | 'yellow' | 'magentaBright' | 'cyanBright'
 
@@ -12,11 +11,6 @@ export default class Logger extends ConsoleLogger {
 
   error(message: any, trace?: string, context?: string) {
     super.error(this.prepareLogMessage(message, 'red'), trace, context)
-
-    sendMessageToAdmin(`⚠️ ${message} | ${trace}`)
-      .catch((error) => {
-        super.error(this.prepareLogMessage('Error while reporting log to telegram', 'red'), error)
-      })
   }
 
   warn(message: any, context?: string) {
